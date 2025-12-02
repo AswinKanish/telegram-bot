8189b51212efe73eb219f2d0c6725f9d9f8c9a75
Aswin kanish <2403717610421075@cit.edu.in>
2025-12-02 21:13:10 +0530

    Add files via upload

---
+++ telegram-bot/main.py
@@ -0,0 +1,20 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    username = user.username if user.username else user.first_name
+    await update.message.reply_text(f"Hi {username} 👋")
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("hi", start))
+    print("Bot is running on Render...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
